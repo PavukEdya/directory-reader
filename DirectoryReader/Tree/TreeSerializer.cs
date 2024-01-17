3c38a162d6d2b49f4c5e44901686ab4048e1d4e3
PavukEdya <slaefir@mail.ru>
2024-01-17 18:53:04 +0500

    create tree representaion in txt file

---
+++ DirectoryReader/Tree/TreeSerializer.cs
@@ -0,0 +1,33 @@
+﻿namespace DirectoryReader.Tree
+{
+    public class TreeSerializer
+    {
+        public IEnumerable<string> Serialize(TreeNode root)
+        {
+            return PrintNode(root,-1);
+        }
+
+        private List<string> PrintNode(TreeNode node,int indentLevel) {
+            var result = new List<string>();
+            if(!IsRoot(node))
+            {
+                var nodeStr = new string(' ', indentLevel) + node.Name;
+                result.Add(nodeStr);
+            }
+            foreach(var child in node.Children)
+            {
+                var childStr = PrintNode(child, indentLevel + 1);
+                foreach(var str in childStr)
+                {
+                    result.Add(str);
+                } 
+            }
+            return result;
+        }
+
+        private bool IsRoot(TreeNode node)
+        {
+            return node.Name == "";
+        }
+    }
+}
