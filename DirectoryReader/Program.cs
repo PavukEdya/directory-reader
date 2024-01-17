64ea565ccc5a49f9f252960f4d34cc95c4443401
PavukEdya <slaefir@mail.ru>
2024-01-17 18:54:49 +0500

    Complete

--- DirectoryReader/Program.cs
+++ DirectoryReader/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DirectoryReader
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            var app = new Application(
+                "Путь до файла чтения",
+                "Путь до файла записи"
+                );
+            app.Run();
+        }
+    }
+}
