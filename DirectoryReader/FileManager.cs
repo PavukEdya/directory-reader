c6c81fec943212acb4d49a1f68f1cc908d5d9ad1
PavukEdya <slaefir@mail.ru>
2024-01-17 18:53:26 +0500

    class to read and write in file

---
+++ DirectoryReader/FileManager.cs
@@ -0,0 +1,20 @@
+﻿namespace DirectoryReader
+{
+    internal class FileManager
+    {
+        private bool CorrectLine(string line)
+        {
+            return line != "";
+        }
+
+        internal IEnumerable<string> GetLinesFromFile(string filePath)
+        {
+            return File.ReadAllLines(filePath).Where(l => CorrectLine(l));
+        }
+
+        internal void WriteLinesInFile(string filePath, IEnumerable<string> lines)
+        {
+            File.WriteAllLines(filePath, lines);
+        }
+    }
+}
