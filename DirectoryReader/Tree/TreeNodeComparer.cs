b1bb7e8399d33beb1292b010375973559470035b
PavukEdya <slaefir@mail.ru>
2024-01-17 18:52:40 +0500

    comparer for TreeNode

---
+++ DirectoryReader/Tree/TreeNodeComparer.cs
@@ -0,0 +1,33 @@
+﻿namespace DirectoryReader.Tree
+{
+    internal class TreeNodeComparer : Comparer<TreeNode>
+    {
+        public override int Compare(TreeNode? x, TreeNode? y)
+        {
+
+            if (x == null && y == null)
+                return 0;
+            if (x == null)
+                return -1;
+            if (y == null)
+                return 1;
+            if (x.Size == null && y.Size == null)
+            {
+                return string.Compare(x.Name, y.Name);
+            }
+            if (x.Size == null)
+            {
+                return -1;
+            }
+            if (y.Size == null)
+            {
+                return 1;
+            }
+            if (x.Size != y.Size)
+            {
+                return y.Size.Value.CompareTo(x.Size.Value);
+            }
+            return string.Compare(x.Name, y.Name);
+        }
+    }
+}
