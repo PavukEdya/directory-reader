1b9b027d49a80ade02a44738fa27e0e88d26e9b5
PavukEdya <slaefir@mail.ru>
2024-01-17 18:51:28 +0500

    class which represent folder or file

---
+++ DirectoryReader/Tree/TreeNode.cs
@@ -0,0 +1,18 @@
+﻿namespace DirectoryReader.Tree
+{
+    public class TreeNode
+    {
+        public TreeNode(string name, int? size = null)
+        {
+            Name = name;
+            Size = size;
+            Children = new SortedSet<TreeNode>(new TreeNodeComparer());
+        }
+
+        public string Name { get; set; }
+
+        public int? Size { get; set; }
+
+        public SortedSet<TreeNode> Children { get; set; }
+    }
+}
